aa28251bca552778705a6889a96e570ffd00938c
Patricio Melo <pmelor@stod.cl>
2024-05-13 00:35:11 -0400

    Inicio

---
+++ Front/component/contact.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-4">
+            <form action="POST" class="form-control">
+                <div class="mb-3">
+                    <label for="Nombre" class="form-label">Nombre</label>
+                    <input type="text" class="form-control" id="Nombre" placeholder="Juan Perez">
+                </div>
+            </form>
+        </div>
+        <div class="col-4">
+            <map name="">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13304.168897091351!2d-70.68116115!3d-33.5262878!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9662db1d0355acf3%3A0xd62dfac41367b16a!2sClub%20Universidad%20de%20Chile!5e0!3m2!1ses-419!2scl!4v1715574692949!5m2!1ses-419!2scl" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </map>
+        </div>
+    </div>
+</div>
