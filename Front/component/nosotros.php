9e9651129a94c3094dc90178f7b5ea2d70172378
CaroVeero <caro.bohle@gmail.com>
2024-05-13 19:43:43 -0400

    Nosotros

--- Front/component/nosotros.php
+++ Front/component/nosotros.php
@@ -1,4 +1,20 @@
-<section>
-    <h1>¿Quiénes somos?</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto nobis, earum nemo ab, reprehenderit accusantium quas inventore ipsam cumque distinctio. Aperiam consectetur laudantium illo sint nesciunt, harum pariatur exercitationem.</p>
-</section>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class=text-align>
+            <h1 >¿Quiénes somos?</h1>
+        </div>
+        <hr>
+        <br><br>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto nobis, earum nemo ab, reprehenderit accusantium quas inventore ipsam cumque distinctio. Aperiam consectetur laudantium illo sint nesciunt, harum pariatur exercitationem.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut hic quidem veritatis distinctio unde molestiae assumenda in atque aperiam, corporis, quae iure maxime officiis sint a mollitia sunt. Aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolore eveniet sint? Est voluptatem inventore a sapiente ea, eveniet accusamus laboriosam nam molestiae, nulla quam deleniti doloribus distinctio, id tempora?</p>
+        </div>
+        <img src="https://realecostate.blob.core.windows.net/reveca/media/uploads/blog/posts/ccaf9c54-d4a7-4672-87b8-1329db2bebe4.jpg" alt="">
+        <br><br>
+        <div>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto nobis, earum nemo ab, reprehenderit accusantium quas inventore ipsam cumque distinctio. Aperiam consectetur laudantium illo sint nesciunt, harum pariatur exercitationem.</p>
+        </div>    
+    </div>
+        <br><br><br><br><br><br>
+</div>
+
