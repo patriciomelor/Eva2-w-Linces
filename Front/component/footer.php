35f5996cc002ef68a840fe59fe64a7260f21cf8b
Patricio Melo <pmelor@stod.cl>
2024-05-13 22:13:11 -0400

    Cambios en footer + Imagen

---
+++ Front/component/footer.php
@@ -0,0 +1,43 @@
+<div class="container">
+  <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+    <div class="col mb-3">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-muted">© 2022</p>
+    </div>
+
+    <div class="col mb-3">
+
+    </div>
+
+    <div class="col mb-3">
+      <h5>Direccion:</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">
+            Entre Talca y La Region de los Lagos
+        </a></li>
+      
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Contacto:</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="tel:950186583" class="nav-link p-0 text-muted">+56 9 5018 6583</a></li>
+        <li class="nav-item mb-2"><a href="wa.link/950186583" class="nav-link p-0 text-muted">Contactanos Al WhatsApp</a></li>
+      </ul>
+    </div>
+
+    <div class="col mb-3">
+      <h5>Redes Sociales</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="https://www.facebook.com/tuparcelaenelsurdechile?mibextid=ZbWKwL" class="nav-link p-0 text-muted"><i class="fa fa-facebook"></i></a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted" disabled>Instagram</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
--- Front/component/nosotros.php
+++ Front/component/nosotros.php
@@ -9,7 +9,7 @@
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto nobis, earum nemo ab, reprehenderit accusantium quas inventore ipsam cumque distinctio. Aperiam consectetur laudantium illo sint nesciunt, harum pariatur exercitationem.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut hic quidem veritatis distinctio unde molestiae assumenda in atque aperiam, corporis, quae iure maxime officiis sint a mollitia sunt. Aliquid! Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolore eveniet sint? Est voluptatem inventore a sapiente ea, eveniet accusamus laboriosam nam molestiae, nulla quam deleniti doloribus distinctio, id tempora?</p>
         </div>
-        <img src="https://realecostate.blob.core.windows.net/reveca/media/uploads/blog/posts/ccaf9c54-d4a7-4672-87b8-1329db2bebe4.jpg" alt="">
+        <img src="../src/media/ImagenParcela.jpeg" alt="">
         <br><br>
         <div>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, architecto nobis, earum nemo ab, reprehenderit accusantium quas inventore ipsam cumque distinctio. Aperiam consectetur laudantium illo sint nesciunt, harum pariatur exercitationem.</p>
