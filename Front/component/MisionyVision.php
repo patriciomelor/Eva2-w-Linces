6a6c6c22edf531a401ffd42b8b2d53b29bad40ec
DanielaDroguett <166835379+DanielaDroguett@users.noreply.github.com>
2024-05-14 22:25:37 -0400

    avances

--- Front/component/MisionyVision.php
+++ Front/component/MisionyVision.php
@@ -1,39 +1,21 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Misión y Visión</title>
-</head>
-<body>
-    <header>
-        <h1>Misión y Visión</h1>
-    </header>
-    <main>
-        <section id="mision">
-            <h2>Misión</h2>
-            <?php
-                // Mision
-                $mision = "Nuestra misión en terrasol Parcelas es ofrecer a nuestros clientes oportunidades unicas para invertir en la naturaleza y tranquilidad.
+<div class="flex-container">
+    <div class="row justify-content-center" >
+        <div>
+            <h1 class="text-center mt-5 mb-3">Mision y Vision?</h1>
+        </div>        
+        <hr>
+        <div class="text-center mb-5">
+            <p>Nuestra misión en terrasol Parcelas es ofrecer a nuestros clientes oportunidades unicas para invertir en la naturaleza y tranquilidad.
                 estamos muy comprometidos con el enfoque innovador y centrado en el cliente,ofrecemos un estilo de vida totalmente rodeado de naturaleza y con las comodidades
-                necesarias para vivir en armonia.";
-   
-                echo "<p>$mision</p>";
-            ?>
-        </section>
-        <section id="vision">
-            <h2>Visión</h2>
-            <?php
-                // Vision
-                $vision = "Nuestra visión es ser reconocidos como lideres en el mercado inmobiliario, destacandonos por nuestro compromiso con la 
+                necesarias para vivir en armonia </p>
+        </div>
+        <div class="text-center" style="max-width: 1200px;">
+            <img src=src/media/C:\Users\C:\Users\Alumno\Desktop.ImagenMisionyVision.jpg>>
+        </div>        
+        <div class="text-center mt-5 mb-5">
+            <p>Nuestra visión es ser reconocidos como lideres en el mercado inmobiliario, destacandonos por nuestro compromiso con la 
                 accesibilidad financiera y la transparencia a cada uno de nuestros clientes. Nos interesa ser confiables y transparentes
-                 en entregar oportunidades realistas";
-                echo "<p>$vision</p>";
-            ?>
-        </section>
-    </main>
-    <footer>
-        <p>Derechos reservados 2024 Terrasol Parcelas</p>
-    </footer>
-</body>
-</html>
+                 en entregar oportunidades realistas</p>
+        </div>    
+    </div>
+</div>
