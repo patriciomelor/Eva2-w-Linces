4f2ae2e877d9d30502e73b95c100087857313ccc
DanielaDroguett <166835379+DanielaDroguett@users.noreply.github.com>
2024-05-14 21:41:31 -0400

    Avance MV

---
+++ Front/component/MisionyVision.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Misión y Visión</title>
+</head>
+<body>
+    <header>
+        <h1>Misión y Visión</h1>
+    </header>
+    <main>
+        <section id="mision">
+            <h2>Misión</h2>
+            <?php
+                // Mision
+                $mision = "Nuestra misión en terrasol Parcelas es ofrecer a nuestros clientes oportunidades unicas para invertir en la naturaleza y tranquilidad.
+                estamos muy comprometidos con el enfoque innovador y centrado en el cliente,ofrecemos un estilo de vida totalmente rodeado de naturaleza y con las comodidades
+                necesarias para vivir en armonia.";
+   
+                echo "<p>$mision</p>";
+            ?>
+        </section>
+        <section id="vision">
+            <h2>Visión</h2>
+            <?php
+                // Vision
+                $vision = "Nuestra visión es ser reconocidos como lideres en el mercado inmobiliario, destacandonos por nuestro compromiso con la 
+                accesibilidad financiera y la transparencia a cada uno de nuestros clientes. Nos interesa ser confiables y transparentes
+                 en entregar oportunidades realistas";
+                echo "<p>$vision</p>";
+            ?>
+        </section>
+    </main>
+    <footer>
+        <p>Derechos reservados 2024 Terrasol Parcelas</p>
+    </footer>
+</body>
+</html>
