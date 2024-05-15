d73770d1128db58b4db095cb40e0d6b7233873f3
DanielaDroguett <166835379+DanielaDroguett@users.noreply.github.com>
2024-05-14 22:31:40 -0400

    avances

--- Front/component/MisionyVision.php
+++ Front/component/MisionyVision.php
@@ -1,10 +1,11 @@
 <div class="flex-container">
     <div class="row justify-content-center" >
         <div>
-            <h1 class="text-center mt-5 mb-3">Mision y Vision?</h1>
+            <h1 class="text-center mt-5 mb-3">Mision y Vision</h1>
         </div>        
         <hr>
         <div class="text-center mb-5">
+            <h3>Mision</h3>
             <p>Nuestra misión en terrasol Parcelas es ofrecer a nuestros clientes oportunidades unicas para invertir en la naturaleza y tranquilidad.
                 estamos muy comprometidos con el enfoque innovador y centrado en el cliente,ofrecemos un estilo de vida totalmente rodeado de naturaleza y con las comodidades
                 necesarias para vivir en armonia </p>
@@ -13,6 +14,7 @@
             <img src=src/media/C:\Users\C:\Users\Alumno\Desktop.ImagenMisionyVision.jpg>>
         </div>        
         <div class="text-center mt-5 mb-5">
+        <h3>Vision</h3>
             <p>Nuestra visión es ser reconocidos como lideres en el mercado inmobiliario, destacandonos por nuestro compromiso con la 
                 accesibilidad financiera y la transparencia a cada uno de nuestros clientes. Nos interesa ser confiables y transparentes
                  en entregar oportunidades realistas</p>
