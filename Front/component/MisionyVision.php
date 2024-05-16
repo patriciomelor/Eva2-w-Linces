ea900abf989e8c2b3a03c8b926e4b7ca66413703
DanielaDroguett <166835379+DanielaDroguett@users.noreply.github.com>
2024-05-15 23:23:38 -0400

    avances

--- Front/component/MisionyVision.php
+++ Front/component/MisionyVision.php
@@ -1,7 +1,7 @@
 <div class="flex-container">
     <div class="row justify-content-center" >
         <div>
-            <h1 class="text-center mt-5 mb-3">Mision y Vision</h1>
+            <h1 class="text-center mt-5 mb-3">MISIÓN y VISIÓN</h1>
         </div>        
         <hr>
         <div class="text-center mb-5">
