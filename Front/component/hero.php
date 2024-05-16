c413db4435b3ee05f9a38aa43a167372fc2dbbb6
Patricio Melo <pmelor@stod.cl>
2024-05-15 22:53:27 -0400

    banner include

---
+++ Front/component/hero.php
@@ -0,0 +1,13 @@
+<div id="carouselExampleSlidesOnly" class="carousel slide w-100" data-ride="carousel">
+  <div class="carousel-inner w-100">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="src/media/banner/1.png" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="src/media/banner/2.png" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="src/media/banner/3.png" alt="Third slide">
+    </div>
+  </div>
+</div>
